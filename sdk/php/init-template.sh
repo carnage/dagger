e5f94a8c2df9807c1890ca605d0a8afea6394b1a
Carnage <t.carnage@gmail.com>
2024-06-06 11:16:33 +0100

    Uppercase class name

--- sdk/php/init-template.sh
+++ sdk/php/init-template.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 cd $1
+CLASS="$(echo -n ${2:0:1} | tr '[:lower:]' '[:upper:]')${2:1}"
 
 if ! [ -f composer.json ]; then
   cp -r /codegen/template/* .
 fi;
 
-sed -i 's/class Example/class $2/g' ./src/Example.php
+sed -i "s/class Example/class $CLASS/g" ./src/Example.php
 
-mv ./src/Example.php ./src/$2.php
+mv ./src/Example.php ./src/$CLASS.php
