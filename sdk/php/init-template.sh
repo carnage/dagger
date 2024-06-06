7c258fc8f190e187543823255cc352e0d9a48c34
Carnage <t.carnage@gmail.com>
2024-06-06 11:30:56 +0100

    Only rename files if we're initialising a new module

--- sdk/php/init-template.sh
+++ sdk/php/init-template.sh
@@ -5,8 +5,7 @@ CLASS="$(echo -n ${2:0:1} | tr '[:lower:]' '[:upper:]')${2:1}"
 
 if ! [ -f composer.json ]; then
   cp -r /codegen/template/* .
+  sed -i "s/class Example/class $CLASS/g" ./src/Example.php
+  mv ./src/Example.php ./src/$CLASS.php
 fi;
 
-sed -i "s/class Example/class $CLASS/g" ./src/Example.php
-
-mv ./src/Example.php ./src/$CLASS.php
